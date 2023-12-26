a8ef02fe0f8040d17b892e7244b07fd7cc0fd15c
Gauravnag <gauravnag28@gmail.com>
2023-12-26 23:52:07 +0530

    child routing, Link route

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ import Body from "./Body";
 import Contact from "./Contact";
 import About from "./About";
 import Error from "./Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const FoodApp = () => {
     return(
         <>
             <Header />
-            <Body />
+            <Outlet />
         </>
     )
 }
@@ -20,16 +20,22 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <FoodApp />,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            }
+        ],
         errorElement: <Error />,
     },
-    {
-        path: "/contact",
-        element: <Contact />,
-    },
-    {
-        path: "/about",
-        element: <About />,
-    }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import {LOGO_URL} from "../utils/constant";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const [btnNameReact, setBtnNameReact] = useState("Login");
@@ -14,9 +15,9 @@ const Header = () => {
                     <img className="logo" src={LOGO_URL}  alt="logo" />
                 </div>
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                     <li>Cart</li>
                     <button onClick={() => btnNameReact === "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login") }>{btnNameReact}</button>
                 </ul>
